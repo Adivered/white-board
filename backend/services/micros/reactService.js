aea55f4f7897ed8760c0f5b6e0c86c357e37ac14
Adivered <akonxd@gmail.com>
2024-12-01 19:32:41 +0100

    fix

--- backend/services/micros/reactService.js
+++ backend/services/micros/reactService.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-let reactController = require('../../handlers/controllers/reactController');
+let { reactController } = require('../../handlers/controllers/reactController');
 
 module.exports = function (app) {
     const router = express.Router();
