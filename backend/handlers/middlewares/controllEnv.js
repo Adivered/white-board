afc0cc25b2e8250ef0945228885e9c085569232c
Adivered <akonxd@gmail.com>
2024-12-01 13:45:42 +0100

    cors

--- backend/handlers/middlewares/controllEnv.js
+++ backend/handlers/middlewares/controllEnv.js
@@ -5,6 +5,7 @@ let cors = require('cors');
 module.exports = (app, env, _dir = __dirname) => {
     if (env == "production") {
         //react-path
+        app.use(cors({ origin: true }));
         app.use(express.static(path.join(_dir, '/build')));
     } else if (env == "development") {
         app.use(cors({ origin: true }));
