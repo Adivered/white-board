fc24e52ba84edd9b221688d2a8ea731021946dd8
Adivered <akonxd@gmail.com>
2024-12-01 19:29:20 +0100

    fix

--- backend/handlers/controllers/reactController.js
+++ backend/handlers/controllers/reactController.js
@@ -4,5 +4,4 @@ module.exports = async (req,res) => {
     // res.send(`<code>App is running on port: ${process.env.PORT}</code>`);
     // res.sendFile(path.join(__dirname + '/index.html'));
     res.sendFile(path.join(__dirname + '/index.html'));
-
 }
